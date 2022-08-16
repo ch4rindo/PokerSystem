a17c09ab486332afd19350d8d46c03b97cdf4b9d
ch4rindo <103165856+ch4rindo@users.noreply.github.com>
2022-08-17 04:56:22 +0900

    Update Deck.php
    ・コードを整形

--- src/charindo/poker/trump/Deck.php
+++ src/charindo/poker/trump/Deck.php
@@ -14,32 +14,9 @@ class Deck {
         $this->max = 52;
     }
 
-    public function initializeDeck() : void{
-        $suits = [
-            CardSuits::SPADE,
-            CardSuits::HEART,
-            CardSuits::DIAMOND,
-            CardSuits::CLUB,
-        ];
-
-        $numbers = [
-            CardNumbers::ACE,
-            CardNumbers::TWO,
-            CardNumbers::THREE,
-            CardNumbers::FOUR,
-            CardNumbers::FIVE,
-            CardNumbers::SIX,
-            CardNumbers::SEVEN,
-            CardNumbers::EIGHT,
-            CardNumbers::NINE,
-            CardNumbers::TEN,
-            CardNumbers::JACK,
-            CardNumbers::QUEEN,
-            CardNumbers::KING,
-        ];
-
-        foreach($numbers as $number) {
-             foreach($suits as $suit) {
+    public function initialize() : void{
+        foreach(CardStore::NUMBERS as $number => $value) {
+             foreach(CardStore::SUITS as $suit => $val) {
                  $this->cards[] = new Card($suit, $number);
              }
         }
@@ -54,17 +31,14 @@ class Deck {
     }
 
     public function shuffle() : array {
-        for($i=0; $i < 5; $i++) {
+        for ($i=0; $i < 5; $i++) {
             shuffle($this->cards);
         }
         return $this->cards;
     }
 
     public function addCard(Card $card) : bool {
-        $temporary_deck = $this->cards;
-        $temporary_deck[] = $card;
-
-        if(!$this->canAddCard($card)) {
+        if (!$this->canAddCard($card)) {
             return false;
         }
 
@@ -75,17 +49,17 @@ class Deck {
     public function canAddCard(Card $card) : bool {
         $target_deck = [];
 
-        foreach($this->cards as $c) {
+        foreach ($this->cards as $c) {
             $target_deck[] = $c->getDescription();
         }
         $target_deck[] = $card->getDescription();
 
         $unique_deck = array_unique($target_deck);
-        if(count($unique_deck) !== count($target_deck)) { //被っているカードが存在する場合
+        if (count($unique_deck) !== count($target_deck)) { //被っているカードが存在する場合
             return false;
         }
 
-        if(count($target_deck) > $this->max){ //デッキの合計枚数が52を超えた場合
+        if (count($target_deck) > $this->max){ //デッキの合計枚数が52を超えた場合
             return false;
         }
 
@@ -93,7 +67,7 @@ class Deck {
     }
 
     public function takeCard() : ?Card {
-        if(count($this->getCards()) <= 0) {
+        if (count($this->getCards()) <= 0) {
             return null;
         }
 
