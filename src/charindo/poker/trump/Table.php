89e3970ffd41ee92872502223e1e9ff4e42f9337
ch4rindo <103165856+ch4rindo@users.noreply.github.com>
2022-08-16 19:36:16 +0900

    Create Table.php
    ・テーブル(場に出されるカード5枚のみ)を作成

---
+++ src/charindo/poker/trump/Table.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace charindo\poker\trump;
+
+class Table {
+
+    protected array $cards;
+    protected int $max;
+
+    public function __construct() {
+        $this->cards = [];
+        $this->max = 5;
+    }
+
+    public function getCards() : array {
+        return $this->cards;
+    }
+
+    public function addCard(Card $card) : bool {
+        if(!$this->canAddCard($card)){
+            $this->cards[] = $card;
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getCount() : int {
+        return count($this->cards);
+    }
+
+    public function canAddCard(Card $card) : bool {
+        $target_table = [];
+
+        foreach($this->cards as $c) {
+            $target_table[] = $c->getDescription();
+        }
+        $target_table[] = $card->getDescription();
+
+        $unique_table = array_unique($target_table);
+        if(count($unique_table) !== count($target_table)) { //被っているカードが存在する場合
+            return false;
+        }
+
+        if(count($target_table) > $this->max){ //テーブルの合計枚数が5を超えた場合
+            return false;
+        }
+
+        return true;
+    }
+}
