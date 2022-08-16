2238328eb1e7a185dc4049e7d5f71127a753e433
ch4rindo <103165856+ch4rindo@users.noreply.github.com>
2022-08-16 19:35:03 +0900

    Create Judgement.php
    ・配役を判定するためのクラスを作成(機能は未実装)
    MEMO: こういう判定クラスの作り方でよいかは知らない。異論は認める

---
+++ src/charindo/poker/trump/Judgement.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace charindo\poker\trump;
+
+class Judgement {
+
+    public static function isOnePair() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isTwoPair() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isThreeOfAKind() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isStraight() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isFlush() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isFullHouse() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isFourOfAKind() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isStraightFlush() : bool {
+        //TODO
+        return false;
+    }
+
+    public static function isRoyalFlush() : bool {
+        //TODO
+        return false;
+    }
+}
